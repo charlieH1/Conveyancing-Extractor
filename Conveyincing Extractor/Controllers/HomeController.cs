236cd187e2bc8d4e9bc8ff115840d051c7dc234b
Charlie Hardy <Chardy@xnlcloud.com>
2026-07-17 18:16:37 +0100

    Add project files.

---
+++ Conveyincing Extractor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Conveyincing_Extractor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Conveyincing_Extractor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
